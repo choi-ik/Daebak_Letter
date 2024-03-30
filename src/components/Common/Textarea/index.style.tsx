d4875d32489a55d45c02dc6a35f0abf283c02465
choi-ik <ci980704@naver.com>
2024-03-30 14:31:22 +0900

    Style: Textarea의 스타일에 className을 추가

--- src/components/Common/Textarea/index.style.tsx
+++ src/components/Common/Textarea/index.style.tsx
@@ -9,40 +9,49 @@ const TextArea = css`
   box-sizing: border-box;
 `;
 
-// width: 100%;
-// height: 20.3125rem;
 export const TextareaContainer = styled.div<{
   darkMode: boolean;
-  width?: string;
-  height?: string;
+  width: string;
+  height: string;
 }>`
+  position: relative;
   background-color: ${(props) =>
     props.darkMode ? props.theme.palette.sub : props.theme.palette.dark};
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: ${(props) => `${props.width};`}
-  height: ${(props) => `${props.height};`}
+  width: ${({ width }) => `${width};`}
+  height: ${({ height }) => `${height};`}
   border-radius: 20px;
   margin-top: 1.625rem;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  ${({ className }) => `${className}`}
 `;
 
-export const TextareaTitle = styled.textarea<{ darkMode: boolean }>`
+export const TextareaTitle = styled.textarea<{
+  darkMode: boolean;
+  width: string;
+  height: string;
+}>`
   ${TextArea};
   color: ${(props) =>
     props.darkMode
       ? props.theme.palette.light_font
       : props.theme.palette.dark_font};
   flex-grow: 0;
-  width: 95%;
-  height: 40px;
+  width: ${({ width }) => `${width};`}
+  height: ${({ height }) => `${height};`}
   font-size: 1rem;
   font-weight: 500;
   padding: 1rem 3rem 0 0.9rem;
+  ${({ className }) => `${className}`}
 `;
 
-export const TextareaContent = styled.textarea<{ darkMode: boolean }>`
+export const TextareaContent = styled.textarea<{
+  darkMode: boolean;
+  width: string;
+  height: string;
+}>`
   ${TextArea};
   color: ${(props) =>
     props.darkMode
@@ -50,13 +59,16 @@ export const TextareaContent = styled.textarea<{ darkMode: boolean }>`
       : props.theme.palette.dark_font};
   flex-grow: 1;
   font-size: 0.7rem;
-  width: 95%;
+  width: ${({ width }) => `${width};`}
+  height: ${({ height }) => (height ? `${height};` : 'auto;')}
   margin: 0.6rem 0;
   padding: 0 1rem;
+  ${({ className }) => `${className}`}
 `;
 
 export const TextareaUnderLine = styled.div`
   width: 90%;
   height: 1px;
   background-color: #aca3a3;
+  ${({ className }) => `${className}`}
 `;
