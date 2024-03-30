6750f326d2cc52ddd82748e4b7c3976dd32af3eb
choi-ik <ci980704@naver.com>
2024-03-30 14:33:21 +0900

    Style: 수정/삭제/완료 버튼 스타일 생성

---
+++ src/components/Common/Textarea/Button/Edit/index.style.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const EditButton = styled.img`
+  position: absolute;
+  right: 2.5rem;
+  bottom: 0.5rem;
+  width: 1.2rem;
+  cursor: pointer;
+  filter: invert(53%) sepia(49%) saturate(669%) hue-rotate(122deg)
+    brightness(95%) contrast(98%);
+`;
