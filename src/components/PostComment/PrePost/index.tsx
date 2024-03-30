6d2202a6535e9300b4cf7ec512a80beb7ed2b40d
choi-ik <ci980704@naver.com>
2024-03-30 14:36:40 +0900

    Refator: 이전에 작성한 포스트를 공통 컴포넌트 Textarea로 변경

--- src/components/PostComment/PrePost/index.tsx
+++ src/components/PostComment/PrePost/index.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form';
 import toast, { Toaster } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import { useAtomValue } from 'jotai';
+import Textarea from '@/components/Common/Textarea';
 import { useCommentDeleteMutation } from '@/hooks/api/useCommentDeleteMutation';
 import { useCreateNotification } from '@/hooks/api/useCreateNotification';
 import { useLikeCreateMutation } from '@/hooks/api/useLikeCreateMutation';
@@ -10,9 +11,7 @@ import { useLikeDeleteMutation } from '@/hooks/api/useLikeDeleteMutation';
 import { usePostDeleteMutation } from '@/hooks/api/usePostDeleteMutation';
 import { usePostUpdateMutation } from '@/hooks/api/usePostUpdateMutation';
 import likeIcon from '@/assets/Like.svg';
-import completeIcon from '@/assets/complete.svg';
 import deleteIcon from '@/assets/delete.svg';
-import editIcon from '@/assets/edit.svg';
 import { idAtom } from '@/store/auth';
 import { Post } from '@/types/ResponseType';
 import * as Style from './index.style';
@@ -38,11 +37,11 @@ function PrePost({ userName, darkMode, postId, postDetail }: PrePostProps) {
   const userId = useAtomValue(idAtom);
 
   const { mutate: likeCreateMutate, data: likeCreateData } =
-    useLikeCreateMutation(postId); // 특정 포스트 좋아요 추가
-  const { mutationLikeDelete } = useLikeDeleteMutation(postId); // 특정 포스트 좋아요 제거
-  const { mutationPostDelete } = usePostDeleteMutation(); // 특정 포스트 제거
-  const { mutationPostUpdate } = usePostUpdateMutation(postId); // 특정 포스트 수정
-  const { mutationCommentDelete } = useCommentDeleteMutation(postId); // 특정 댓글 제거
+    useLikeCreateMutation(postId);
+  const { mutationLikeDelete } = useLikeDeleteMutation(postId);
+  const { mutationPostDelete } = usePostDeleteMutation();
+  const { mutationPostUpdate } = usePostUpdateMutation(postId);
+  const { mutationCommentDelete } = useCommentDeleteMutation(postId);
   const { mutate: notificationMutate } = useCreateNotification();
   const navigator = useNavigate();
 
@@ -57,7 +56,7 @@ function PrePost({ userName, darkMode, postId, postDetail }: PrePostProps) {
     }
   });
 
-  const [postState, setPostState] = useState(false); // 상단 포스트 편집 여부 상태
+  const [isEdit, setIsEdit] = useState(true);
 
   /** 포스트 좋아요 추가 함수 */
   const handleLikeCreateClick = async () => {
@@ -84,17 +83,16 @@ function PrePost({ userName, darkMode, postId, postDetail }: PrePostProps) {
     }
   };
 
-  /** 특정 포스트 수정 함수 */
-  const handlePostToggleClick = () => {
-    setPostState((state) => !state);
+  const handleEditToggleClick = () => {
+    setIsEdit((state) => !state);
   };
 
-  /** 특정 포스트 삭제 함수 */
   const handleDeletePostClick = () => {
     if (userName === '익명') {
       toast.error('익명 회원은 편지를 삭제 할 수 없습니다.');
       return;
     }
+
     const deleteCheck = confirm(
       '편지를 삭제 하시겠습니까? 편지를 삭제하시면 편지를 포함한 모든 댓글도 함께 삭제됩니다.'
     );
@@ -108,26 +106,24 @@ function PrePost({ userName, darkMode, postId, postDetail }: PrePostProps) {
     }
   };
 
-  /** 특정 댓글 삭제하는 함수 */
   const handleDeleteCommentClick = (e: React.MouseEvent<HTMLImageElement>) => {
     if (userName === '익명') {
       toast.error('익명 회원은 댓글을 삭제할 수 없습니다.');
       return;
     }
+
     const deleteCheck = confirm('댓글 삭제하시겠습니까?');
     if (deleteCheck) {
       const targetElement = e.target as HTMLElement;
       const commentId = targetElement.dataset.id;
 
-      if (commentId) {
+      commentId &&
         mutationCommentDelete({
           id: commentId
         });
-      }
     }
   };
 
-  /** 포스트 수정 내용 서버 전송 함수 */
   const onSubmit = (submitData: userFormProps) => {
     mutationPostUpdate({
       postId,
@@ -137,70 +133,57 @@ function PrePost({ userName, darkMode, postId, postDetail }: PrePostProps) {
       color: postDetail ? JSON.parse(postDetail.title).color : ''
     });
 
-    handlePostToggleClick();
+    handleEditToggleClick();
   };
 
   useEffect(() => {
-    likeCreateData &&
+    if (likeCreateData) {
       notificationMutate({
         notificationType: 'LIKE',
         notificationTypeId: likeCreateData._id,
         userId: postDetail.author._id,
         postId: postDetail._id
       });
-  }, [likeCreateData]);
+    }
 
-  useEffect(() => {
     if (isSubmitting) {
       errors.prePostContent
         ? toast.error(errors.prePostContent.message as string)
         : null;
     }
-  }, [isSubmitting]);
+  }, [likeCreateData, isSubmitting]);
 
   return (
     <>
       <Style.PrePostAndCommentContainer>
-        <Style.PrePostContainer darkMode={darkMode}>
-          <Style.PrePostInnerTitle darkMode={darkMode}>
-            {postDetail && JSON.parse(postDetail.title).title}
-          </Style.PrePostInnerTitle>
-          <Style.PrePostUnnerline />
-          {postState ? (
-            <Style.PrePostEditContent
-              darkMode={darkMode}
-              defaultValue={postDetail && JSON.parse(postDetail.title).content}
-              {...register('prePostContent', {
-                required: '편지 내용은 반드시 입력해야 합니다.'
-              })}
-            />
-          ) : (
-            <Style.PrePostContent darkMode={darkMode}>
-              {postDetail && JSON.parse(postDetail.title).content}
-            </Style.PrePostContent>
-          )}
-          {postState ? (
-            <Style.CompleteImg
-              src={completeIcon}
-              onClick={handleSubmit(onSubmit)}
-            />
-          ) : (
-            <Style.EditImg
-              src={editIcon}
-              onClick={() => {
-                if (userName === '익명') {
-                  toast.error('익명 회원은 편지를 수정 할 수 없습니다.');
-                  return;
-                }
-                handlePostToggleClick();
-              }}
-            />
-          )}
-          <Style.DeleteImg
-            src={deleteIcon}
-            onClick={handleDeletePostClick}
+        <Textarea
+          width={'100%'}
+          height={'13.3125rem'}>
+          <Textarea.TextareaTitle
+            readonly={true}
+            value={postDetail && JSON.parse(postDetail.title).title}
+            maxLength={15}
+            width={'95%'}
+            height={'40px'}
           />
-        </Style.PrePostContainer>
+          <Textarea.TextareaUnderLine />
+          <Textarea.TextareaContent
+            readonly={isEdit}
+            placeholder={'내용을 입력하세요'}
+            register={register}
+            formKey={'prePostContent'}
+            registerOptions={{
+              required: '내용을 반드시 입력해야합니다.'
+            }}
+            width={'95%'}
+            height={'3rem'}
+          />
+          {isEdit && <Textarea.EditButton onClick={handleEditToggleClick} />}
+          {!isEdit && (
+            <Textarea.CompleteButton onClick={handleSubmit(onSubmit)} />
+          )}
+          <Textarea.DeleteButton onClick={handleDeletePostClick} />
+        </Textarea>
         <Style.LikeCommentContainer>
           <Style.LikeLogoContainer onClick={handleLikeCreateClick}>
             <Style.LikeLogo src={likeIcon} />
